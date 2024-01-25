fc21b8e2f0caa8c97f9166e72388c541300c731f
mdk-1 <59763792+mdk-1@users.noreply.github.com>
2024-01-25 13:19:50 +1100

    update

--- src/bot.ts
+++ src/bot.ts
@@ -159,20 +159,31 @@ const aboutUrlKeyboard = new InlineKeyboard().url(
 
 // Suggest commands in the menu
 bot.api.setMyCommands([
-  { command: "yo", description: "Be greeted by the bot" },
-  {
-    command: "effect",
-    description: "Apply text effects on the text. (usage: /effect [text])",
+    { command: "info", description: "HEY THERE! Just to let you know, you can use / sanickart, /sanicdeathmatch, /saniccomic, and / future to learn more.Follow the URL to the Sanic The Hedgehog website where you will find the whitepaper and tokenomics as well as a breakdown of the critical aspects of project Sanicverse: https://www.sanicthehedghog.com" },
+    {
+        command: "sanickart",
+        description: "Follow the URL to play the Sanic Kart Open Beta: https://sanickart.sanicthehedgehog.com",
+
+        command: "sanicdeathmatch",
+        description: "Sanic Deathmatch is coming soon!",
+
+        command: "saniccomic",
+        description: "Sanic Comic is coming soon!",
+
+        command: "future",
+        description: "Keep checking back often, watch our socials on Twitter/X and Telegram because the Sanicverse will continue to be built out with new utility and dApps.",
   },
 ]);
 
 // Handle all other messages and the /start command
-const introductionMessage = `Hello! I'm a Telegram bot.
-I'm powered by Cyclic, the next-generation serverless computing platform.
+const introductionMessage = `Hello! I am Sanic AI - I can help you navigate the Sanicverse.
 
 <b>Commands</b>
-/yo - Be greeted by me
-/effect [text] - Show a keyboard to apply text effects to [text]`;
+/info - Be greeted by me
+/sanickart - Follow the URL to play the Sanic Kart Open Beta: https://sanickart.sanicthehedgehog.com
+/sanicdeathmatch - Sanic Deathmatch is coming soon!
+/saniccomic - Sanic Comic is coming soon!
+/future - Keep checking back often, watch our socials on Twitter/X and Telegram because the Sanicverse will continue to be built out with new utility and dApps.`;
 
 const replyWithIntro = (ctx: any) =>
   ctx.reply(introductionMessage, {
